fc994164eabfc9fedc87be0d06f7ea0bc239b4f4
Jason Ruan <jason.ruan@lytx.com>
2021-08-11 14:15:28 -0700

    initial checkin

---
+++ Algorithms/Algorithms/LinkedLists/ListNode.cs
@@ -0,0 +1,13 @@
+﻿namespace Algorithms.LinkedLists
+{
+    public class ListNode<T>
+    {
+        public ListNode(T data, ListNode<T> next){
+            Data = data;
+            Next = Next;
+        }
+
+        public T Data { get; set; }
+        public ListNode<T> Next { get; set; }        
+    }
+}
---
+++ Algorithms/Algorithms/Trees/TreeNode.cs
@@ -0,0 +1,18 @@
+﻿namespace Algorithms.Trees
+{
+    public class TreeNode<T>
+    {
+        public TreeNode(T data, TreeNode<T> left, TreeNode<T> right)
+        {
+            Data = data;
+            Left = left;
+            Right = right;
+        }
+
+        public T Data { get; set; }
+
+        public TreeNode<T> Left { get; set; }
+
+        public TreeNode<T> Right { get; set; }
+    }
+}
