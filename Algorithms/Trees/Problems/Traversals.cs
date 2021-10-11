4d83935f3fc07243c01076e2bf37dc70f5d43bfd
Jason Ruan <jason.ruan@lytx.com>
2021-10-11 15:57:21 -0700

    tree traversals

--- Algorithms/Trees/Problems/Traversals.cs
+++ Algorithms/Trees/Problems/Traversals.cs
@@ -1,10 +1,130 @@
-﻿using System;
+﻿using DataStructures;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Trees.Problems
 {
-    class Traversals
+    public class Traversals
     {
+        public void InOrderTraversalRecursively(TreeNodes<int> root)
+        {
+            if (root == null)
+            {
+                return;
+            }
+
+            InOrderTraversalRecursively(root.Left);
+            Console.Write(root.Data + " ");
+            InOrderTraversalRecursively(root.Right);
+        }
+
+        public void InOrderTraversalIterative(TreeNodes<int> root)
+        {
+            if (root == null)
+            {
+                return;
+            }
+
+            var stack = new Stack<TreeNodes<int>>();
+
+            var curr = root;
+            while(stack.Count > 0 || curr != null)
+            {
+                if (curr != null)
+                {
+                    stack.Push(curr);
+                    curr = curr.Left;
+                }
+                else
+                {
+                    curr = stack.Pop();
+                    Console.Write(curr.Data + " ");
+                    curr = curr.Right;
+                }                
+            }
+        }
+
+        public void PreOrderTraversalRecursively(TreeNodes<int> root)
+        {
+            if (root == null)
+            {
+                return;
+            }
+
+            Console.Write(root.Data + " ");
+            PreOrderTraversalRecursively(root.Left);
+            PreOrderTraversalRecursively(root.Right);
+        }
+
+        public void PreOrderTraversalIterative(TreeNodes<int> root)
+        {
+            if (root == null)
+            {
+                return;
+            }
+
+            var stack = new Stack<TreeNodes<int>>();
+            stack.Push(root);
+            
+            while(stack.Count > 0)
+            {
+                var node = stack.Pop();
+                Console.Write(node.Data + " ");
+                if (node.Right != null)
+                {
+                    stack.Push(node.Right);
+                }
+
+                if (node.Left != null)
+                {
+                    stack.Push(node.Left);
+                }               
+            }
+        }
+
+        public void PostOrderTraversalRecursively(TreeNodes<int> root)
+        {
+            if(root == null)
+            {
+                return;
+            }
+
+            PostOrderTraversalRecursively(root.Left);
+            PostOrderTraversalRecursively(root.Right);
+            Console.Write(root.Data + " ");
+        }
+
+        public void PostOrderTraversalIterative(TreeNodes<int> root)
+        {
+            if (root == null)
+            {
+                return;
+            }
+            
+            var s1 = new Stack<TreeNodes<int>>();
+            var s2 = new Stack<TreeNodes<int>>();
+
+            s1.Push(root);
+            while(s1.Count > 0)
+            {
+                var node = s1.Pop();
+                s2.Push(node);
+                if (node.Left != null)
+                {
+                    s1.Push(node.Left);
+                }
+
+                if (node.Right != null)
+                {
+                    s1.Push(node.Right);
+                }
+            }
+
+            while (s2.Count > 0)
+            {
+                Console.Write(s2.Pop().Data + " ");
+            }
+        }
     }
 }
--- Algorithms/Trees/Program.cs
+++ Algorithms/Trees/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DataStructures;
+using System;
+using Trees.Problems;
 
 namespace Trees
 {
@@ -6,7 +8,44 @@ namespace Trees
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //TreeNodes<int> root = new TreeNodes<int>(10, null, null);
+            //TreeNodes<int> left = new TreeNodes<int>(20, null, null);
+            //TreeNodes<int> right = new TreeNodes<int>(30, null, null);
+            //TreeNodes<int> leftLeft = new TreeNodes<int>(40, null, null);
+            //TreeNodes<int> leftLeftLeft = new TreeNodes<int>(70, null, null);
+            //TreeNodes<int> leftRight = new TreeNodes<int>(50, null, null);
+            //TreeNodes<int> rightLeft = new TreeNodes<int>(60, null, null);
+            //TreeNodes<int> leftLeftRight = new TreeNodes<int>(80, null, null);
+
+            //root.Left = left;
+            //root.Right = right;
+            //left.Left = leftLeft;
+            //leftLeft.Left = leftLeftLeft;
+            //left.Right = leftRight;
+            //right.Left = rightLeft;
+            //leftLeft.Right = leftLeftRight;
+
+            //var traversal = new Traversals();
+            //traversal.PreOrderTraversalIterative(root);
+            //Console.WriteLine("\n");
+            //traversal.PreOrderTraversalRecursively(root);
+
+            TreeNodes<int> root = new TreeNodes<int>(1, null, null);
+            root.Left = new TreeNodes<int>(2, null, null);
+            root.Right = new TreeNodes<int>(3, null, null);
+            root.Left.Left = new TreeNodes<int>(4, null, null);
+            root.Right.Left = new TreeNodes<int>(5, null, null);
+            root.Right.Right = new TreeNodes<int>(6, null, null);
+            root.Right.Left.Left = new TreeNodes<int>(7, null, null);
+            root.Right.Left.Right = new TreeNodes<int>(8, null, null);
+
+            var traversal = new Traversals();
+            //traversal.InOrderTraversalIterative(root);
+            //Console.WriteLine("\n");
+            //traversal.InOrderTraversalRecursively(root);
+            traversal.PostOrderTraversalIterative(root);
+            Console.WriteLine("\n");
+            traversal.PostOrderTraversalRecursively(root);
         }
     }
 }
